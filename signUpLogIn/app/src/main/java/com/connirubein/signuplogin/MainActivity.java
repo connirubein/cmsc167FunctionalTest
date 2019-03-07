dff78b25a9fdb6c18675083b3f3db91d158e6b48
connirubein <connirubeinbaliog@gmail.com>
2019-03-07 22:38:32 +0800

    Add files via upload

---
+++ signUpLogIn/app/src/main/java/com/connirubein/signuplogin/MainActivity.java
@@ -0,0 +1,64 @@
+package com.connirubein.signuplogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import android.content.Intent;
+import android.support.annotation.VisibleForTesting;
+//import android.support.test.espresso.IdlingResource;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Declaration Button
+    private Button buttonSignup;
+    private Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("welcooooooooooooooooooooome");
+        initViews();
+        initListeners();
+    }
+
+    private View.OnClickListener btnOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btnSignup:
+                    openSignUpActivity();
+                    break;
+                case R.id.btnLogin:        //go directly to map activity after logging in
+                    openLogInActivity();
+                    break;
+            }
+        }
+    };
+
+    private void initViews(){
+        buttonLogin = findViewById(R.id.btnLogin);
+        buttonSignup = findViewById(R.id.btnSignup);
+    }
+
+    private void initListeners(){
+        buttonSignup.setOnClickListener(btnOnClickListener);
+        buttonLogin.setOnClickListener(btnOnClickListener);
+    }
+
+    private void openSignUpActivity(){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+    private void openLogInActivity(){
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
+    }
+
+}
